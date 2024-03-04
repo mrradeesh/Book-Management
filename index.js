1c132660d60759253b77b26018de87f3ee23d84d
mrradeesh <mrradeesh18@gmail.com>
2024-03-04 16:13:28 +0530

    adding files

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+// const { users } = require("./data/users.json");
+const userRoute = require("./routes/users");
+const bookRoute = require("./routes/books");
+//const kill = require("kill-port");
+
+const app = express();
+const PORT = 8080;
+//kill(8080, "tcp");
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.status(200).json({
+    success: true,
+    message: "Server started",
+  });
+});
+
+app.use("/users", userRoute);
+app.use("/books", bookRoute);
+app.get("*", (req, res) => {
+  res.status(501).json({
+    Error: "INVALID PATH",
+    message: "not implimented yet",
+  });
+});
+
+app.listen(PORT, () => {
+  console.log("server started running on port ", PORT);
+});
